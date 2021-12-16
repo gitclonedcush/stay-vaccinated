0def93da610430fc4f412e6356c9efdc6c1097c9
Dan Cushing <dan.cushing@divvypay.com>
2021-12-15 21:34:51 -0700

    updating map

--- vaccination_map.py
+++ vaccination_map.py
@@ -36,13 +36,11 @@ def read_data():
 
 
 def build_vaccinations_map(vaccinations_current):
-	max_population = vaccinations_current['total_population'].max()
-	scale = max_population / 100
+	vaccinations_current['hover_text'] = 'Location: ' + vaccinations_current['location'] + '<br>Population: ' + vaccinations_current["total_population"].astype(str) + '<br>Percent Vaccinated: ' + vaccinations_current["percent_vaccinated"].astype(str)
 
-	# colors = ['#fea889', '#f16a6f', '#def2f1', '#91d4d1', '#205566']
 	layout = {
 		'title': go.layout.Title({
-			'text': 'Global Covid Vaccinations',
+			'text': 'Global Covid Vaccination Rates',
 			'font': {
 				'color': 'white'
 			}
@@ -56,13 +54,14 @@ def build_vaccinations_map(vaccinations_current):
 		),
 		'height': 1000,
 		'width': 1400,
-		'paper_bgcolor': 'rgba(17,17,17,1)',
+		'plot_bgcolor': 'rgba(0,0,0,0.9)',
+		'paper_bgcolor': 'rgba(0,0,0,0.9)',
 		'geo': go.layout.Geo({
-			'bgcolor': 'rgba(17,17,17,1)',
+			'bgcolor': 'rgba(0,0,0,0.9)',
 			'showocean': True,
 			'oceancolor': 'rgba(51,153,255,0.9)',
 			'showland': True,
-			'landcolor': 'rgb(2,48,32,1)',
+			'landcolor': 'rgb(40,40,43,1)',
 			'resolution': 50,
 			'scope': 'world',
 			'showframe': True,
@@ -71,19 +70,152 @@ def build_vaccinations_map(vaccinations_current):
 			'projection_type': 'orthographic',
 		}),
 	}
+	colors = ['rgba(108,40,117,1)', 'rgba(129,92,143,1)', 'rgba(163,137,173,1)', 'rgba(202,183,201,1)', 'rgba(224,224,224,1)',
+		'rgba(184,207,181,1)', 'rgba(161,203,151,1)', 'rgba(77,162,91,1)', 'rgba(19,108,53,1)', 'rgba(8,93,23,1)']
 
-	fig = go.Figure(layout=layout)
+	data = [
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'] < 10]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'] < 10]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'] < 10]['hover_text'],
+			name = '<10',
+			mode = 'markers',
+			marker=dict(
+				size=10,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[1]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'].between(10, 20, inclusive='left')]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'].between(10, 20, inclusive='left')]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'].between(10, 20, inclusive='left')]['hover_text'],
+			name = '10-20',
+			mode = 'markers',
+			marker=dict(
+				size=15,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[2]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'].between(20, 30, inclusive='left')]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'].between(20, 30, inclusive='left')]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'].between(20, 30, inclusive='left')]['hover_text'],
+			name = '20-30',
+			mode = 'markers',
+			marker=dict(
+				size=20,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[3]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'].between(30, 40, inclusive='left')]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'].between(30, 40, inclusive='left')]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'].between(30, 40, inclusive='left')]['hover_text'],
+			name = '30-40',
+			mode = 'markers',
+			marker=dict(
+				size=25,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[4]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'].between(40, 50, inclusive='left')]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'].between(40, 50, inclusive='left')]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'].between(40, 50, inclusive='left')]['hover_text'],
+			name = '40-50',
+			mode = 'markers',
+			marker=dict(
+				size=30,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[5]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'].between(50, 60, inclusive='left')]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'].between(50, 60, inclusive='left')]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'].between(50, 60, inclusive='left')]['hover_text'],
+			name = '50-60',
+			mode = 'markers',
+			marker=dict(
+				size=35,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[6]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'].between(60, 70, inclusive='left')]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'].between(60, 70, inclusive='left')]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'].between(60, 70, inclusive='left')]['hover_text'],
+			name = '60-70',
+			mode = 'markers',
+			marker=dict(
+				size=40,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[6]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'].between(70, 80, inclusive='left')]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'].between(70, 80, inclusive='left')]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'].between(70, 80, inclusive='left')]['hover_text'],
+			name = '70-80',
+			mode = 'markers',
+			marker=dict(
+				size=45,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[7]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'].between(80, 90, inclusive='left')]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'].between(80, 90, inclusive='left')]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'].between(80, 90, inclusive='left')]['hover_text'],
+			name = '80-90',
+			mode = 'markers',
+			marker=dict(
+				size=50,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[8]
+			)
+		),
+		go.Scattergeo(
+			locationmode = 'ISO-3',
+			locations = vaccinations_current[vaccinations_current['percent_vaccinated'] >= 90]['iso_code'],
+			text = vaccinations_current[vaccinations_current['percent_vaccinated'] >= 90]['percent_vaccinated'],
+			hovertext = vaccinations_current[vaccinations_current['percent_vaccinated'] >= 90]['hover_text'],
+			name = '>=90',
+			mode = 'markers',
+			marker=dict(
+				size=np.log1p(vaccinations_current[vaccinations_current['percent_vaccinated'] >= 90]['percent_vaccinated'])**2,
+				sizemode='area',
+				opacity=0.9,
+				color = colors[9]
+			)
+		),
+	]
 
-	for i, row in vaccinations_current.iterrows():
-		fig.add_trace(go.Scattergeo(
-				locationmode = 'ISO-3',
-				locations = [row.iso_code],
-				text = row.percent_vaccinated,
-				name = row.location,
-				mode = 'markers',
-				marker=dict(
-					size=np.log1p(row.percent_vaccinated)**2,
-					sizemode='area',
-				)))
+	fig = go.Figure(data=data, layout=layout)
 
 	return fig
